47618f137ab0deeee36c9504e07f3c2d90ca7637
kennethlng <ng.kennethlee@gmail.com>
2020-03-04 17:18:31 -0800

    Updated to use new database model

--- functions/index.js
+++ functions/index.js
@@ -7,105 +7,110 @@ admin.initializeApp();
 const db = admin.firestore(); 
 
 exports.createItem = functions.firestore
-    .document("items/{item}")
+    .document("collections/{collection}/collectionItems/{collectionItem}")
     .onCreate((snap, context) => {
         const data = snap.data(); 
+        const createdOn = snap.createTime; 
+        const createdBy = data.createdBy; 
 
         // Add createdOn and createdBy to document
         return snap.ref.set({
-            createdOn: snap.createTime,
-            createdBy: data.createdBy,
-            modifiedOn: snap.createTime,
-            modifiedBy: data.createdBy
-        }, {merge: true})
-        .then(() => {
-            // Create a new doc mapping the new item to the use that created the item
-            return db.collection("userToItem").add({
-                item: {
-                    id: snap.id,
-                    link: data.link,
-                    title: data.title,
-                    description: data.description,
-                    stealth: data.stealth
-                },
-                user: {
-                    id: data.createdBy
-                },
-                createdOn: snap.createTime,
-                modifiedOn: snap.createTime,
-            })
-        })
+            createdOn,
+            modifiedOn: createdOn,
+            modifiedBy: createdBy
+        }, { merge: true })
     })
 
-exports.updateCollection = functions.firestore
-    .document("collections/{collectionId}")
-    .onUpdate((change, context) => {
-        const data = change.after.data(); 
-        const previousData = change.before.data(); 
-        if (data) {
-            if (data.name === previousData.name && data.description === previousData.description) {
-                return null   
-            }
+// exports.updateCollection = functions.firestore
+//     .document("collections/{collectionId}")
+//     .onUpdate((change, context) => {
+//         const data = change.after.data(); 
+//         const previousData = change.before.data(); 
+//         if (data) {
+//             if (data.name === previousData.name && data.description === previousData.description) {
+//                 return null   
+//             }
 
-            return null; 
+//             return null; 
 
-            // var batch = db.batch(); 
+//             // var batch = db.batch(); 
 
-            // // Update userToCollection documents
-            // const getDocs = db.collection("userToCollection").where("collection.id", "==", context.params.collectionId).get()
-            // return getDocs.then(function(querySnapshot) {
-            //     querySnapshot.forEach(function(doc) {
-            //         var ref = doc.ref; 
-            //         batch.update(ref, { 
-            //             collection: {
-            //                 name: data.name, 
-            //                 description: data.description
-            //             }
-            //         })
-            //     })
+//             // // Update userToCollection documents
+//             // const getDocs = db.collection("userToCollection").where("collection.id", "==", context.params.collectionId).get()
+//             // return getDocs.then(function(querySnapshot) {
+//             //     querySnapshot.forEach(function(doc) {
+//             //         var ref = doc.ref; 
+//             //         batch.update(ref, { 
+//             //             collection: {
+//             //                 name: data.name, 
+//             //                 description: data.description
+//             //             }
+//             //         })
+//             //     })
 
-            //     return batch.commit()
-            // })
-            // .catch(error => console.log(error))
-        } else {
-            return null; 
-        }
-    })
+//             //     return batch.commit()
+//             // })
+//             // .catch(error => console.log(error))
+//         } else {
+//             return null; 
+//         }
+//     })
 
 exports.createCollection = functions.firestore
     .document("collections/{collectionId}")
     .onCreate((snapshot, context) => {
         const data = snapshot.data(); 
+        const name = data.name; 
+        const createdOn = snapshot.createTime; 
+        const createdBy = data.createdBy;  
 
         return snapshot.ref.set({
-            createdOn: snapshot.createTime,
-            createdBy: data.createdBy,
-            modifiedOn: snapshot.createTime,
-            modifiedBy: data.createdBy
-        }, {merge: true})
+            active: true, 
+            createdOn,
+            createdBy,
+            modifiedOn: createdOn,
+            modifiedBy: createdBy
+        }, { merge: true })
         .then(() => {
-            return db.collection("users").doc(data.createdBy).collection("userCollections").doc(snapshot.id).set({
-                id: snapshot.id,
-                name: data.name,
-                createdOn: snapshot.createTime,
-                createdBy: data.createdBy,
-                modifiedOn: snapshot.createTime,
-                modifiedBy: data.createdBy
-            })
+            return db.collection("collections").doc(snapshot.id).collection("collectionUsers").doc(data.createdBy)
+                .set({
+                    modifiedOn: createdOn
+                })
+                .then(() => {
+                    return db.collection("users").doc(createdBy).collection("userCollections").doc(snapshot.id)
+                        .set({
+                            name,
+                            modifiedOn: createdOn,
+                            modifiedBy: createdBy,
+                            createdOn,
+                            createdBy
+                        })
+                })
+
+            // return db.collection("users").doc(data.createdBy).collection("userCollections").doc(snapshot.id).set({
+            //     id: snapshot.id,
+            //     name: data.name,
+            //     createdOn: snapshot.createTime,
+            //     createdBy: data.createdBy,
+            //     modifiedOn: snapshot.createTime,
+            //     modifiedBy: data.createdBy
+            // })
 
             // // Create a new doc mapping the new item to the use that created the item
             // return db.collection("userToCollection").add({
             //     collection: {
             //         id: snapshot.id,
             //         name: data.name,
+            //         visibility,
+            //         secret
             //     },
             //     user: {
-            //         id: data.createdBy
+            //         id: createdBy
             //     },
-            //     createdOn: snapshot.createTime,
-            //     createdBy: data.createdBy,
-            //     modifiedOn: snapshot.createTime,
-            //     modifiedBy: data.createdBy
+            //     createdOn,
+            //     createdBy,
+            //     modifiedOn: createdOn,
+            //     modifiedBy: createdBy
             // })
         })
     })
