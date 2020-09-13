4292ef009afc97535d6674419bf6f6cc31fd5967
kennethlng <ng.kennethlee@gmail.com>
2020-09-13 00:16:08 -0700

    Updated constants to reflect new fields

--- functions/constants/database.js
+++ functions/constants/database.js
@@ -1,39 +1,21 @@
-const USERS = "users";
-const FOLDERS = "folders";
-const FILES = "files"; 
-const USERS_FOLDERS = "usersFolders";
-const FOLDERS_FILES = "foldersFiles"; 
-const MODIFIED_ON = "modifiedOn";
-const MODIFIED_BY = "modifiedBy"; 
-const CREATED_ON = "createdOn"; 
-const CREATED_BY = "createdBy"; 
-const FOLDER_ID = "folderID"; 
-const FOLDER_TITLE = "folderTitle"; 
-const FILE_ID = "fileID"; 
-const FILE_TITLE = "fileTitle";
-const USER_ID = "userID"; 
-
 module.exports = {
-    USERS,
-    FOLDERS,
-    FILES,
-    USERS_FOLDERS,
-    FOLDERS_FILES,
-    MODIFIED_ON,
-    MODIFIED_BY,
-    CREATED_ON,
-    CREATED_BY,
-    FOLDER_ID,
-    FOLDER_TITLE,
-    FILE_ID,
-    FILE_TITLE,
-    FILE_IMAGE_URL: "fileImageURL",
-    USER_ID,
+    USERS: "users",
+    FOLDERS: "folders",
+    FILES: "files",
+    USER_FOLDERS: "user_folders",
+    FOLDER_FILES: "folder_files",
+    FOLDER_USERS: "folder_users",
+    MODIFIED_ON: "modified_on",
+    MODIFIED_BY_USER_ID: "modified_by_user_id",
+    CREATED_ON: "created_on",
+    CREATED_BY_USER_ID: "created_by_user_id",
+    FOLDER_ID: "folder_id",
+    FILE_ID: "file_id",
+    USER_ID: "user_id",
     LINK: "link",
-    IMAGE_URL: "imageURL",
+    IMAGE_URL: "image_url",
     TITLE: "title",
     DESCRIPTION: "description",
     FAVICON: "favicon",
-    SHARE_LINK: "shareLink",
-    CREATED_BY_USER_ID: "createdByUserID"
+    SHARE_LINK: "share_link",
 }
